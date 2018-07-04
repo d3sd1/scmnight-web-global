b95bda17a8e67817ba95e2abc8415f28b4daf5c6
Jause <andirexulon@gmail.com>
2018-07-04 22:06:24 +0200

    websockets funcionando singletons inicializados router creado proyecto creado auth guard iniciado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {LoggedinModule} from './loggedin/loggedin.module';
+import {LoggedoutModule} from './loggedout/loggedout.module';
+import {WebsocketsModule} from './websockets/websockets.module';
+import {WebsocketsConnectorService} from './websockets/websockets-connector.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    WebsocketsModule.forRoot(),
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LoggedinModule,
+    LoggedoutModule
   ],
-  providers: [],
+  exports: [
+    BrowserAnimationsModule
+  ],
+  providers: [WebsocketsConnectorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
